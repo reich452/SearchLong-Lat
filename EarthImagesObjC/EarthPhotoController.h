121977bbc3e109b15a2325586d5e3fe65be84db2
reich452 <reich452@regis.edu>
2017-03-16 14:51:56 -0600

    Added model and MVC fiels

---
+++ EarthImagesObjC/EarthPhotoController.h
@@ -0,0 +1,13 @@
+//
+//  EarthPhotoController.h
+//  EarthImagesObjC
+//
+//  Created by Nick Reichard on 3/16/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EarthPhotoController : NSObject
+
+@end
---
+++ EarthImagesObjC/EarthPhotoController.m
@@ -0,0 +1,13 @@
+//
+//  EarthPhotoController.m
+//  EarthImagesObjC
+//
+//  Created by Nick Reichard on 3/16/17.
+//  Copyright © 2017 Nick Reichard. All rights reserved.
+//
+
+#import "EarthPhotoController.h"
+
+@implementation EarthPhotoController
+
+@end
